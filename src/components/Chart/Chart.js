adc6ba4d2d76eb734026552ef95404b3acd1d7f2
Peter Maged <petermaged222@gmail.com>
2022-07-02 01:01:52 +0200

    uploading main project's files

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,21 @@
+import ChartBars from "./ChartBars";
+import './Chart.css';
+
+const Chart = ({dataPoints}) => {
+
+    const dataPointValues = dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointValues);
+
+    return <div className="chart">
+        {dataPoints.map(dataPoint => (
+            <ChartBars
+            key={dataPoint.label}
+            value={dataPoint.value}
+            maxValue={totalMaximum}
+            label = {dataPoint.label}
+            />
+        ))}
+    </div>
+};
+ 
+export default Chart;
